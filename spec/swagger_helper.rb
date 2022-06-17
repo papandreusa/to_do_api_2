86455093b4064f5173854e704ccae498f7fcac1b
Andrii Yurchenko <papandreus.a@gmail.com>
2022-06-17 20:02:44 +0300

    fix rswag

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -24,10 +24,10 @@ RSpec.configure do |config|
       paths: {},
       servers: [
         {
-          url: 'http://{defaultHost}',
+          url: '{defaultHost}',
           variables: {
             defaultHost: {
-              default: '127.0.0.1:3000'
+              default: 'https://127.0.0.1:3000'
             }
           }
         }
